3cc72c34fc8b3327953f9e5570fac32a01442ff7
Baachi <info@bachi.biz>
2015-02-03 22:38:15 +0100

    Add MappingStep which now uses symfony/property-accesor under the hood. This simplyfied the code and is a lot faster.

---
+++ src/Ddeboer/DataImport/Step/MappingStep.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ddeboer\DataImport\Step;
+
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+class MappingStep implements StepInterface
+{
+    private $mappings;
+
+    public function __construct(array $mappings = [], PropertyAccessor $accessor = null)
+    {
+        $this->mappings = $mappings;
+        $this->accessor = $accessor ?: new PropertyAccessor();
+    }
+
+    public function map($from, $to)
+    {
+        $this->mappings[$from] = $to;
+
+        return $this;
+    }
+
+    public function process(&$item)
+    {
+        foreach ($this->mappings as $from => $to) {
+            $value = $this->accessor->getValue($item, $from);
+            $this->accessor->setValue($item, $to, $value);
+        }
+    }
+} 
